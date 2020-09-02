72200175f3ef7f023e8d0eb0f8ee0a8c53cd6b7f
Pedro <pedrohfsd@gmail.com>
2020-09-02 16:27:25 -0300

    Removing query from erp url.

--- dotnet/Constants.cs
+++ dotnet/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public const string ErpPricesUrl = "http://erp.partner.com/quote?token";
+        public const string ErpPricesUrl = "http://erp.partner.com/quote";
         
     }
 }
